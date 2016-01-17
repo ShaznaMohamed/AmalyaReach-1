2941021a850773c68c405ccc19109a4daad6bcf7
vishwnv <vish_v@live.com>
2016-01-17 12:58:56 +0530

    footer eke poddak :laughing:
    dishan ara login tka mekata add krnwda.. mage athin e tka nathi una...
    and colors wenas krnna owa kynna

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,7 @@
-@extends('layouts.app')
+@extends('templates.default')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <h3>Hello Amalyaa</h3>
+        <p>Homepage is here !!!</p>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@stop
---
+++ resources/views/templates/partials/navigation.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" >AmalyaReach</a>
+        </div>
+        
+        <div class="collpase navbar-collapse">
+        
+        </div>
+             
+    </div>
+
+</nav>
