b6a279928a2642658ee3390d251ba160ffbe9ec4
Savithra <it14118982@my.sliit.lk>
2016-01-18 16:42:12 +0530

    Style tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
-
+<style>
        .fa-btn {
             margin-right: 6px;
         }
